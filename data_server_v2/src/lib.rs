1c799e36d24b8ec0c652fca3e2459a69cb9e57d4
Brian <bnheise@gmail.com>
2023-10-01 14:15:22 +0900

    added instance extraction middleware

--- data_server_v2/src/lib.rs
+++ data_server_v2/src/lib.rs
@@ -2,6 +2,7 @@ use actix_web::{
     web::{self, Data},
     App, HttpServer,
 };
+use middleware::virtual_hosting::VirtualHostProcessor;
 use routes::{health_check, instance::create_instance};
 use std::{io, net::TcpListener};
 use surrealdb::{engine::any::Any, Surreal};
@@ -9,13 +10,17 @@ use tracing_actix_web::TracingLogger;
 
 pub mod configuration;
 pub mod database;
+mod middleware;
 pub mod model;
 mod routes;
 pub mod telemetry;
 
 pub async fn run(listener: TcpListener, db: Surreal<Any>) -> io::Result<()> {
+    // TODO: create instance guard to handle directing to instance handling or main admin instance
+    // TODO: set up proper tracing logs for existing endpoints and middleware
     HttpServer::new(move || {
         App::new()
+            .wrap(VirtualHostProcessor)
             .wrap(TracingLogger::default())
             .route("/health_check", web::get().to(health_check))
             .route("/instance", web::post().to(create_instance))
---
+++ data_server_v2/src/middleware/virtual_hosting/mod.rs
@@ -0,0 +1,97 @@
+use actix_web::{
+    dev::{forward_ready, Service, ServiceRequest, ServiceResponse, Transform},
+    Error, HttpMessage,
+};
+
+use futures_util::future::LocalBoxFuture;
+use futures_util::FutureExt;
+use std::{
+    fmt::Display,
+    future::{ready, Ready},
+    rc::Rc,
+};
+
+pub struct VirtualHostProcessor;
+
+impl<S, B> Transform<S, ServiceRequest> for VirtualHostProcessor
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error> + 'static,
+{
+    type Response = ServiceResponse<B>;
+    type Error = Error;
+    type Transform = VirtualHostMiddleware<S>;
+    type InitError = ();
+    type Future = Ready<Result<Self::Transform, Self::InitError>>;
+
+    fn new_transform(&self, service: S) -> Self::Future {
+        ready(Ok(VirtualHostMiddleware {
+            service: Rc::new(service),
+        }))
+    }
+}
+
+pub struct VirtualHostMiddleware<S> {
+    service: Rc<S>,
+}
+
+impl<S, B> Service<ServiceRequest> for VirtualHostMiddleware<S>
+where
+    S: Service<ServiceRequest, Response = ServiceResponse<B>, Error = Error> + 'static,
+{
+    type Response = ServiceResponse<B>;
+
+    type Error = Error;
+
+    type Future = LocalBoxFuture<'static, Result<Self::Response, Self::Error>>;
+
+    forward_ready!(service);
+
+    fn call(&self, req: ServiceRequest) -> Self::Future {
+        let srv = self.service.clone();
+
+        async move {
+            let instance: Result<InstanceName, _> = req.connection_info().host().try_into();
+
+            if let Ok(instance) = instance {
+                req.extensions_mut().insert(instance);
+            }
+
+            let res = srv.call(req).await?;
+
+            Ok(res)
+        }
+        .boxed_local()
+    }
+}
+
+#[derive(Debug)]
+struct InstanceName(String);
+
+impl Display for InstanceName {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.0)
+    }
+}
+
+impl TryFrom<&str> for InstanceName {
+    type Error = &'static str;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        let mut period_count: u8 = 0;
+        let mut instance_name = None;
+        for (idx, char) in value.chars().enumerate() {
+            if char == '.' {
+                if period_count == 0 {
+                    instance_name = Some(InstanceName(value[0..idx].into()));
+                }
+                period_count += 1
+            }
+        }
+
+        if period_count == 2 {
+            return instance_name.ok_or("Failed to parse the host");
+        }
+
+        Err("Failed to parse the host")
+    }
+}
