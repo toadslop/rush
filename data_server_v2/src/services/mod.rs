030bb05ab15026d11562af5dfd29e0e779e4ab20
Brian <bnheise@gmail.com>
2023-10-25 16:21:07 +0900

    updated health check to return unhealthy if database or smtp server cannot be reached

--- data_server_v2/src/services/mod.rs
+++ data_server_v2/src/services/mod.rs
@@ -1,11 +1,35 @@
-use actix_web::HttpResponse;
+use actix_web::{web, HttpResponse};
+use futures_util::join;
+use lettre::{AsyncSmtpTransport, Tokio1Executor};
+use surrealdb::{engine::any::Any, Surreal};
 
 pub mod instance;
 pub mod root;
 mod util;
 
-#[tracing::instrument(name = "Health check requested")]
-pub async fn health_check() -> HttpResponse {
+#[tracing::instrument(name = "Health check")]
+pub async fn health_check(
+    db: web::Data<Surreal<Any>>,
+    mailer: web::Data<AsyncSmtpTransport<Tokio1Executor>>,
+) -> HttpResponse {
     tracing::debug!("Health check requested");
-    HttpResponse::Ok().finish()
+    let (db_health, mailer_health) = join!(async { db.health().await }, mailer.test_connection());
+
+    match (db_health, mailer_health) {
+        (Ok(_), Ok(_)) => HttpResponse::Ok().finish(),
+        (Ok(_), Err(e)) => {
+            tracing::error!("Smtp Server connection is unhealthy: {e}");
+            HttpResponse::InternalServerError().body("Failed to connect to smtp server")
+        }
+        (Err(e), Ok(_)) => {
+            tracing::error!("Database connection is unhealthy: {e}");
+            HttpResponse::InternalServerError().body("Failed to connect to database server")
+        }
+        (Err(db_e), Err(smtp_e)) => {
+            tracing::error!("Smtp Server connection is unhealthy: {smtp_e}");
+            tracing::error!("Database connection is unhealthy: {db_e}");
+            HttpResponse::InternalServerError()
+                .body("Failed to connect to database server and to mail server")
+        }
+    }
 }
